05bdf888097b68f4a05da0826b865db301e167be
MyWebAura <mywebaura@gmail.com>
2024-05-03 01:10:22 +0500

    Added content of About

--- scripts/autoType.js
+++ scripts/autoType.js
@@ -5,3 +5,10 @@ var typed = new Typed(".auto-type", {
   looped: true,
   loop: true,
 });
+var typed = new Typed(".auto-type2", {
+  strings: ["Web Developer...", "Front-end Developer..."],
+  typeSpeed: 95,
+  backSpeed: 95,
+  looped: true,
+  loop: true,
+});
