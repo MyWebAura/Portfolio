7ed787b13b46125818b0bddc6f1976fdafd47805
MyWebAura <mywebaura@gmail.com>
2024-05-03 16:46:03 +0500

    Creating Project Section

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const mobileViewBtn = document.getElementById("mobile-view-btn");
+const videoPlayer = document.getElementById("videoPlayer");
+const projectVideo = document.querySelector(".project-video")
+function closeVideo(){
+    projectVideo.pause();
+    projectVideo.currentTime = 0;
+    videoPlayer.classList.add("d-none");
+}
+function openVideo(file){
+    projectVideo.src= file;
+    videoPlayer.classList.remove("d-none");
+}
