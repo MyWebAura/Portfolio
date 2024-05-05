630c2cfbe63029966efb54e4aaecd2fdfabc39c3
MyWebAura <mywebaura@gmail.com>
2024-05-05 14:53:28 +0500

    Creating skillprogress bar animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,24 @@
 const mobileViewBtn = document.getElementById("mobile-view-btn");
 const videoPlayer = document.getElementById("videoPlayer");
-const projectVideo = document.querySelector(".project-video")
-function closeVideo(){
-    projectVideo.pause();
-    projectVideo.currentTime = 0;
-    videoPlayer.classList.add("d-none");
+const projectVideo = document.querySelector(".project-video");
+function closeVideo() {
+  projectVideo.pause();
+  projectVideo.currentTime = 0;
+  videoPlayer.classList.add("d-none");
 }
-function openVideo(file){
-    projectVideo.src= file;
-    videoPlayer.classList.remove("d-none");   
+function openVideo(file) {
+  projectVideo.src = file;
+  videoPlayer.classList.remove("d-none");
 }
 document.addEventListener("scroll", () => {
-    const navBar = document.querySelector(".navbar");
-    const offNavBar = document.querySelector(".off-navbar");
-    if (window.scrollY > 0) {
-      navBar.classList.add("navbar-bg");
-      offNavBar.classList.add("offcanvas-bg");
-      offNavBar.classList.add("offcanvas-bg");
-    } else {
-      navBar.classList.remove("navbar-bg");
-      offNavBar.classList.remove("offcanvas-bg");
-    }
-  });
+  const navBar = document.querySelector(".navbar");
+  const offNavBar = document.querySelector(".off-navbar");
+  if (window.scrollY > 0) {
+    navBar.classList.add("navbar-bg");
+    offNavBar.classList.add("offcanvas-bg");
+    offNavBar.classList.add("offcanvas-bg");
+  } else {
+    navBar.classList.remove("navbar-bg");
+    offNavBar.classList.remove("offcanvas-bg");
+  }
+});
