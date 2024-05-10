188e32c8eb07298fa9310beab6e5284e0e67d716
MyWebAura <mywebaura@gmail.com>
2024-05-11 00:13:58 +0500

    Modified php

--- php/sendEmail.php
+++ php/sendEmail.php
@@ -2,13 +2,13 @@
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get form data
-    $name = $_POST["nameInput"];
-    $email = $_POST["emailInput"];
-    $message = $_POST["messageInput"];
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
     // Email parameters
     $to = "mywebaura@gmail.com"; // Change this to your email address
-    $subject = $message;
+    $subject = "New Message from Contact Form";
     $body = "Name: $name\nEmail: $email\nMessage: $message";
 
     // Send email
